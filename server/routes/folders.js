cc92e90dbb69799446acadbd5528cb3380ba9fa4
subham <subham301477@gmail.com>
2019-03-27 11:39:27 +0530

    Add: Access based routes

--- server/routes/folders.js
+++ server/routes/folders.js
@@ -3,7 +3,6 @@ import hash from 'object-hash';
 
 import verifyUser from '../VerifyUser';
 import Folder from '../models/folders';
-import XList from '../models/xlist';
 import Notes from '../models/notes';
 
 // eslint-disable-next-line new-cap
@@ -48,7 +47,7 @@ app.post('/create', auth, (req, res) => {
       };
       // Generate unique id for the folder
       createFolder.id = hash(createFolder);
-      createFolder.path = `${result.path}$${createFolder.id}`;
+      createFolder.path = `${result.path}\\$${createFolder.id}`;
 
       const newFolder = new Folder(createFolder);
       return newFolder.save();
@@ -83,127 +82,6 @@ app.get('/get/:id', auth, (req, res) => {
 });
 
 /*
- * get all the users who has specified access to specified folder
- * for getting the read-only accessible use
- * 'type' = 'read' and for read-write use 'type' = 'write'
- */
-app.get('/access/:type/:id', auth, (req, res) => {
-  if (req.params.type !== 'read' && req.params.type !== 'write')
-    return res.status(404).json({ error: 'Invalid end-point' });
-
-  Folder.findOne({ id: req.params.id })
-    .then(folder => {
-      if (!folder)
-        return res
-          .status(400)
-          .json({ folder: "Requested folder doesn't exists" });
-
-      const type = req.params.type === 'read' ? 0 : 1;
-      const result = folder.xlist.filter(x => x.visibility === type);
-      return res.json({ xlist: result });
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-  return 0;
-});
-
-/*
- * give the specified access to a particular user
- * for read-only access use 'type' = 'read' and for
- * read-write access use 'type' = 'write'
- *
- * body-parameters : email[](array of email-id (may be empty))-required param
- *                   xlist(xlist-name (not a mandatory param))
- */
-app.post('/access/:type/:id', auth, (req, res) => {
-  if (req.params.type !== 'read' && req.params.type !== 'write')
-    return res.status(404).json({ error: 'Invalid end-point' });
-
-  // check whether the body parameters are sent correctly or not
-  if (!req.body.email || !Array.isArray(req.body.email))
-    return res.status(400).json({
-      email:
-        'email field must be an array of emails of user and is a required parameter'
-    });
-
-  const usersList = req.body.email;
-  const type = req.params.type === 'read' ? 0 : 1;
-
-  const folderPromise = Folder.findOne({ id: req.params.id });
-  // if xlist is present then check for it's validity
-  if (req.body.xlist) {
-    const xlistPromise = XList.findOne({
-      name: req.body.xlist,
-      owner: req.user.email
-    });
-    Promise.all([xlistPromise, folderPromise])
-      .then(([xlist, folder]) => {
-        if (!xlist) {
-          res.status(400).json({ xlist: "Mentinoed xlist doesn't exists" });
-          return null;
-        }
-        if (!folder) {
-          res.status(400).json({ folder: "Mentioned folder doesn't exists" });
-          return null;
-        }
-
-        for (let i = 0; i < xlist.members.length; i += 1)
-          usersList.push(xlist.members[i]);
-
-        const userListWithPermission = usersList.map(x => {
-          return {
-            email: x,
-            visibility: type
-          };
-        });
-
-        const regexString = `^${folder.path}[a-zA-Z0-9$]*`;
-        return Folder.update(
-          { owner: req.user.id, path: { $regex: new RegExp(regexString) } },
-          { $push: { xlist: { $each: userListWithPermission.slice() } } }
-        );
-      })
-      .then(result => {
-        if (!result) return;
-        res.send('Successfully updated!');
-      })
-      .catch(err => {
-        return res.status(500).send(err);
-      });
-    return 0;
-  }
-
-  folderPromise
-    .then(folder => {
-      if (!folder) {
-        res.status(400).json({ folder: "Mentioned folder doesn't exists" });
-        return null;
-      }
-      const userListWithPermission = usersList.map(x => {
-        return {
-          email: x,
-          visibility: type
-        };
-      });
-      const regexString = `^${folder.path}[a-zA-Z0-9$]*`;
-      return Folder.update(
-        { owner: req.user.id, path: { $regex: new RegExp(regexString) } },
-        { $push: { xlist: { $each: userListWithPermission.slice() } } }
-      );
-    })
-    .then(result => {
-      if (!result) return;
-      res.send('Successfully updated');
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-
-  return 0;
-});
-
-/*
  * delete specified folder
  */
 app.delete('/delete/:id', auth, (req, res) => {
@@ -216,7 +94,7 @@ app.delete('/delete/:id', auth, (req, res) => {
         return null;
       }
 
-      regex = new RegExp(`^${folder.path}`.replace(/\$/g, '\\$'));
+      regex = new RegExp(`${folder.path}`);
       /* console.log(regex); */
       return Folder.deleteMany({ path: regex });
     })
