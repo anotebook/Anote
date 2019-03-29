742915a5613945b54deec0c576791287d38ccb5d
subham <subham301477@gmail.com>
2019-03-29 23:43:52 +0530

    fix: Invalid prop-warnig

--- client/src/reducers/widthChanged.js
+++ client/src/reducers/widthChanged.js
@@ -4,7 +4,7 @@ const initialState = 0;
 
 export default (prvState = initialState, action) => {
   if (action.type === WIDTH_CHANGED) {
-    return !prvState;
+    return prvState === 0 ? 1 : 0;
   }
   return prvState;
 };
